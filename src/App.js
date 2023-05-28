68901841210a6c9f3ffdc7fe5168050c8e4e81ce
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-05-27 20:57:05 -0500

    dark mode button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { useState } from 'react'
 
 import Sidebar from './components/Sidebar'
 import Content from './components/Content'
@@ -6,11 +6,15 @@ import Content from './components/Content'
 import './styles/App.scss'
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
   return (
-    <Fragment>
-      <Sidebar />
+    <div className={darkMode ? 'dark' : 'light'}>
+      <Sidebar
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+      />
       <Content />
-    </Fragment>
+    </div>
   )
 }
 
