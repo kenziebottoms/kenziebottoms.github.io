f1840526b7a3a1a58319c6dee8bf977b3e3c1de2
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-08-11 18:30:40 -0500

    move loading icon sass

--- src/elements/LoadingIcon.jsx
+++ src/elements/LoadingIcon.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 import icon from '../img/loading.svg'
 
-import '../styles/elements/LoadingIcon.scss'
-
 const LoadingIcon = () => 
   <div id='loading'>
     <img src={icon} alt='Loading...' />
