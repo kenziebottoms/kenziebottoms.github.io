d8ee6e2a8e1a94e37a16de6b7c438b1a40b6dd1f
Kenzie Bottoms <kenziebottoms@gmail.com>
2026-08-11 18:18:21 -0500

    fix dark mode feat coloring

--- src/elements/Feat.jsx
+++ src/elements/Feat.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const Feat = ({ icon, title, children }) => 
-  <div className='px-4 py-3 bg-black-hl flex flex-row gap-4 items-center'>
-    <i className='dark:text-black-hl bg-black dark:bg-green p-2 text-3xl rounded-full material-icons'>{icon}</i>
+  <div className='px-4 py-3 bg-white dark:bg-black-hl flex flex-row gap-4 items-center'>
+    <i className='text-black bg-green p-2 text-3xl rounded-full material-icons'>{icon}</i>
     <div>
       <h3 className='text-xl font-semibold'>{title}</h3>
       {children}
